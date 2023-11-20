ce59fb53e42a5d406d7fc3e3e5efac120f6a931a
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-19 22:22:23 -0500

    clarified app.js, index.js & notfound.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage';
 import Congress from './Congress';
 import Parliament from './Parliament';
-import NotFound from './NotFound'; //Import NotFound component
+import NotFound from './NotFound'; //Import NotFound component for handling unknown routes
 
+//so here BrowserRouter is set up as the main router, theres multiple routes with two routes being nested and the error handling in case path is not valid ie 404s
 function App() { // added error handling component
   return (
     <div className="App">
