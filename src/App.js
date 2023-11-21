7704983cb0b174df6d7564b41c3fe29876cdaa2b
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-11-21 15:39:03 -0500

    temp removed nested routes

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ const basename = process.env.REACT_APP_BASENAME || '/';
 
 //so here BrowserRouter is set up as the main router, theres multiple routes with two routes being nested and the error handling in case path is not valid ie 404s
 function App() { // added error handling component
+  console.log('Basename:', process.env.REACT_APP_BASENAME);
+
   return (
     <div className="App">
       <Router basename={basename}>
         <Routes>
-          <Route path="/API-app" element={<Homepage />} />
-          <Route path="/API-app/congress" element={<Congress />} />
-          <Route path="/API-app/parliament" element={<Parliament />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/congress" element={<Congress />} />
+          <Route path="/parliament" element={<Parliament />} />
           <Route path="*" element={<NotFound/>} /> {} 
         </Routes>
       </Router>
